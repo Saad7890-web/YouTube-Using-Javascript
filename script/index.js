b79d0c9cdf63571c54209a0e717f450222551a22
Saad7890-web <omysaadislam@gmail.com>
2025-12-01 15:06:59 +0600

    dynamically loaded the video container

--- script/index.js
+++ script/index.js
@@ -6,6 +6,12 @@ function loadCategories() {
         .then(data => displayCategories(data.categories))
 }
 
+function loadVideos(){
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => displayVideos(data.videos));
+}
+
 function displayCategories(categories) {
     const categoryContainer = document.getElementById('category-container');
 
@@ -20,4 +26,60 @@ function displayCategories(categories) {
     }
 }
 
+const displayVideos = videos => {
+    const videoContainer = document.getElementById('video-container');
+
+    videos.forEach(video => {
+        // console.log(video);
+        const videoCard = document.createElement('div');
+        videoCard.innerHTML = `
+    <div class="card bg-base-100">
+        <figure class="relative">
+          <img class="w-full h-[150px] object-cover" src="${
+            video.thumbnail
+          }" alt="Shoes" />
+          <span
+            class="absolute bottom-2 right-2 text-sm rounded text-white bg-black px-2"
+            >3hrs 56 min ago</span
+          >
+        </figure>
+
+        <div class="flex gap-3 px-0 py-5">
+          <div class="profile">
+            <div class="avatar">
+              <div
+                class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2"
+              >
+                <img
+                  src="${video.authors[0].profile_picture}"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="intro">
+            <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+            <p class="text-sm text-gray-400 flex gap-1">
+             ${video.authors[0].profile_name}
+              ${
+                video.authors[0].verified == true
+                  ? `<img
+                class="w-5 h-5"
+                src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"
+                alt=""
+              />`
+                  : ``
+              }
+            </p>
+            <p class="text-sm text-gray-400">${video.others.views} views</p>
+          </div>
+
+        
+        `;
+
+        videoContainer.append(videoCard);
+    })
+}
+
 loadCategories();
+loadVideos();
